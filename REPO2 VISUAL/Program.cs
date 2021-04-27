1f8d9fa5aa17687ccd92b06af13a4dd5d750a9ed
Antonio_Keyxer <antonioguanilogil@gmail.com>
2021-04-26 19:58:22 -0500

    Agregar archivos de proyecto.

---
+++ REPO2 VISUAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace REPO2_VISUAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
